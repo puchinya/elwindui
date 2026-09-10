46869f3241bb2d9f7754c9e03420c51e3f251ad0
puchinya <puchinya@daradara.jp>
2026-09-10 09:42:44 +0900

    test(animation-demo): keep native exit observable

--- examples/animation-demo/src/main.rs
+++ examples/animation-demo/src/main.rs
@@ -45,7 +45,7 @@ mod animation_demo_view_model {
 
         fn toggle_native(&self) {
             elwindui::core::ui::with_animation(
-                Animation::ease_out(Duration::from_millis(220)),
+                Animation::ease_out(Duration::from_millis(1200)),
                 || {
                     show_native = !show_native;
                 },
